155a8c546fab090193c4ef408a7d1a58a861b56a
honeyni <ninon.matidika@edu.esiee-it.fr>
2025-12-01 10:09:31 +0100

    Implement main function for directory reading
    Add main function to read directory name from command line

--- TP5/src/repertoire_main.c
+++ TP5/src/repertoire_main.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include "repertoire.h"
 
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Utilisation : %s <nom_du_repertoire>\n", argv[0]);
+        return 1;
+    }
+
+    char *nom = argv[1];
+    lire_dossier(nom);
+
+    return 0;
+}
