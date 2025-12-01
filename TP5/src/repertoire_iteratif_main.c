3b81b19ceaf23e70eee264e14e31035aa0eaa215
honeyni <ninon.matidika@edu.esiee-it.fr>
2025-12-01 10:45:13 +0100

    Create repertoire_iteratif_main.c

---
+++ TP5/src/repertoire_iteratif_main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "repertoire.h"
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Utilisation : %s <nom_du_repertoire>\n", argv[0]);
+        return 1;
+    }
+
+    lire_dossier_iteratif(argv[1]);
+
+    return 0;
+}
